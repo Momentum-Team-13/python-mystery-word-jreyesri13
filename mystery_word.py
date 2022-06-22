e26644bde67b38fc76d08f50eacced7b77ae8205
Jose Reyes <jreyesri@cbu.edu>
2022-06-22 09:47:36 -0400

    Start of game working

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,36 @@
+import random
+# from nbformat import read
+
+
 def play_game():
-    pass
+    read_file = read_text_file()
+    random_word = get_random_word(read_file)
+    start_of_game(random_word)
+
+
+def read_text_file():
+    text_file = "words.txt"
+    with open(text_file) as open_file:
+        read_file = open_file.read()
+
+    word_list = read_file.split()
+
+    return word_list
+
+
+def get_random_word(words):
+    words_length = len(words)
+    random_number = random.randrange(0, words_length)
+    random_word = words[random_number]
+
+    return random_word
+
+
+def start_of_game(word):
+    word_length = len(word)
+    print(f"The word has {word_length} characters.")
+    print("_" * word_length)
+    print(f"Answer = {word}")
 
 
 if __name__ == "__main__":
